2ee17f52916ea9e9a317113ecd9c9658df4fc3c4
joelin97 <joelin97@gmail.com>
2018-03-12 10:49:37 +0000

    Create FistFrame.java

---
+++ swing/FistFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+public class FistFrame extends JFrame{
+  public BaiduFrame(){
+    this.setTitle();
+    this.setBounds();
+    this.setSize();
+    this.setLocation();
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setVisible(true);
+    }
+    public static void main(String[] args)
+      FistFrame ff = new FistFrame();
+    }
+}
